5623369a99628f00f556b2bb21a4429d362182e6
prndcb <psatanique@gmail.com>
2025-01-31 17:17:20 +0100

    Update botinfo.js

--- commands/bot/botinfo.js
+++ commands/bot/botinfo.js
@@ -18,18 +18,18 @@ module.exports = {
             const embed = new Discord.MessageEmbed()
 
             embed.setTitle(`Information à Propos De : ${client.user.username}`)
-            embed.setURL('https://discord.gg/9ZfB8m5E88')
+            embed.setURL('https://discord.gg/paranoid-x')
             embed.setDescription('**Une description complète et détaillée du Bot** <@1225087364821684379>')
             embed.setColor(color)
             .setThumbnail(message.author.avatarURL({ dynamic:true }));
             embed.setTimestamp()
             embed.setFooter(`${client.config.name}`)
             embed.addFields(
-                { name: '👑 Owner / Developer :', value: '<@1208337813339373569>', inline: true },
+                { name: '👑 Owner:', value: 'mzk le plus bo', inline: true },
                 { name: '🔌 Latence Ping Bot :', value: `\`${client.ws.ping}Ms\`` },
                 { name: '🚀 Total Server(s) :', value: `\`${client.guilds.cache.size}\``, inline: true },
                 { name: '👥 Total User(s) :', value: `\`${client.guilds.cache.reduce((a, b) => a + b.memberCount, 0)}\``, inline: true },
-                { name: '📞 Support :', value: `[Clique ICI Pour Rejoindre Le Support](https://discord.gg/htNuh5pZ)`, inline: true },
+                { name: '📞 Support :', value: `[Support](https://discord.gg/paranoid-x)`, inline: true },
                 { name: '📗 Node.js Version :', value: `\`${process.version}\``, inline: true },
                 { name: "📚 Discord.js Version :", value: `\`${Discord.version}\``, inline: true },
                 { name: "🟢 Uptime :", value: `<t:${(Date.now()-client.uptime).toString().slice(0, -3)}:R>`, inline: true }, 
